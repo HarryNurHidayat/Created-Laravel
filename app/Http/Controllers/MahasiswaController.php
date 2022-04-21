ad832b943764a5a3a5688402ec88324c596a3b18
Harry Nur <harry.nur.ft19@mail.umy.ac.id>
2022-04-21 12:26:01 +0700

    Menambahkan fungsi delete

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -9,7 +9,7 @@ class MahasiswaController extends Controller
     public function index()
     {
         $data_mahasiswa = \App\Models\Mahasiswa::all();
-        return view('mahasiswa.index',['data_mahasiswa' => $data_mahasiswa]);
+        return view('mahasiswa.index', ['data_mahasiswa' => $data_mahasiswa]);
     }
 
     // Fungsi dari code diatas yaitu pembuatan function Create dan kemduian data yang 
@@ -17,18 +17,26 @@ class MahasiswaController extends Controller
     public function create(Request $request)
     {
         \App\Models\Mahasiswa::create($request->all());
-        return redirect('/mahasiswa')->with('sukses','Data berhasil di input');
+        return redirect('/mahasiswa')->with('sukses', 'Data berhasil di input');
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
         $data_mahasiswa = \App\Models\Mahasiswa::find($id);
-        return view('mahasiswa.edit',['mahasiswa'=>$data_mahasiswa]);
-    
+        return view('mahasiswa.edit', ['mahasiswa' => $data_mahasiswa]);
     }
 
-    public function update(Request $request, $id){
+    public function update(Request $request, $id)
+    {
         $data_mahasiswa = \App\Models\Mahasiswa::find($id);
         $data_mahasiswa->update($request->all());
         return redirect('mahasiswa')->with('sukses', 'Data Berhasil diupdate');
+    }
+
+    public function delete($id)
+    {
+        $data_mahasiswa = \App\Models\Mahasiswa::find($id);
+        $data_mahasiswa->delete();
+        return redirect('/mahasiswa')->with('sukses', 'Data berhasil dihapus');
     }
 }
